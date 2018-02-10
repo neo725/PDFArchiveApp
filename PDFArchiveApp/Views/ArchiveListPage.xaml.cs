d6a3162552afd12df6e6cde0077f07fefd6bba31
Neo Chang <neo725@gmail.com>
2018-02-11 01:46:37 +0800

    加入專案檔案。

---
+++ PDFArchiveApp/Views/ArchiveListPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using PDFArchiveApp.Models;
+using PDFArchiveApp.Services;
+
+using Windows.UI.Xaml.Controls;
+
+namespace PDFArchiveApp.Views
+{
+    public sealed partial class ArchiveListPage : Page, INotifyPropertyChanged
+    {
+        // TODO WTS: Change the grid as appropriate to your app.
+        // For help see http://docs.telerik.com/windows-universal/controls/raddatagrid/gettingstarted
+        // You may also want to extend the grid to work with the RadDataForm http://docs.telerik.com/windows-universal/controls/raddataform/dataform-gettingstarted
+        public ArchiveListPage()
+        {
+            InitializeComponent();
+        }
+
+        public ObservableCollection<SampleOrder> Source
+        {
+            get
+            {
+                // TODO WTS: Replace this with your actual data
+                return SampleDataService.GetGridSampleData();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
